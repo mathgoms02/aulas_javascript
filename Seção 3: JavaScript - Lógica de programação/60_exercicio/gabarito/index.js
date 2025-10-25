dcc99780827334d7df82a60d61b42ebc73fd805a
mathgoms02 <math.gomsx3@gmail.com>
2025-10-24 23:59:21 -0300

    Aulas 24-10-2025
    Summary:
    - Refatorado

---
+++ Seção 3: JavaScript - Lógica de programação/60_exercicio/gabarito/index.js
@@ -0,0 +1,11 @@
+// Algumas formas
+
+function max(x, y){
+    if (x > y){
+        return x;
+    }
+    return y;
+}
+
+// Utilizando operador ternário também, igual vc fez
+// Mas vou fazer arrow function
---
+++ Seção 3: JavaScript - Lógica de programação/60_exercicio/index.js
@@ -0,0 +1,6 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+function max_number(num1, num2){
+    return num1 >= num2 ? num1 : num2;
+}
+
+console.log(max_number(2, 1))
